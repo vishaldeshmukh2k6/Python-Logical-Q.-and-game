38b426acf54673226b44a39ade0a958a4859894b
vishaldeshmuk143@gmail.com <vishaldeshmuk143@gmail.com>
2025-02-22 23:14:16 +0530

    game

---
+++ LogicalQuestion/game.py
@@ -0,0 +1,15 @@
+# 1.  guess a number ?
+
+number = 100
+guess = 0
+attempt = 0
+
+while guess != number:
+   guess = int(input("Guess a Number :- "))
+   attempt = attempt + 1
+print("you win in",attempt,"attempt")
+print("Your guess a Right!")
+
+
+
+# 2.  
